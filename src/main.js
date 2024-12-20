161ce44016216b195eb5f72e122bfbdc01bb8be4
Darren <darren0801@163.com>
2024-12-20 15:02:32 +0800

    显示全部的控件

--- src/main.js
+++ src/main.js
@@ -23,6 +23,12 @@ function setRemUnit() {
 setRemUnit();
 window.addEventListener('resize', setRemUnit);
 
+// main.js中引入以下为bpmn工作流绘图工具的样式
+import 'bpmn-js/dist/assets/diagram-js.css' // 左边工具栏以及编辑节点的样式
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+
 
 new Vue({
   router,
--- src/views/processDesign/components/bpmn2.js
+++ src/views/processDesign/components/bpmn2.js
@@ -7,14 +7,14 @@ export default class CustomPaletteProvider extends PaletteProvider {
   getPaletteEntries(element) {
     const entries = super.getPaletteEntries(element);
     // 隐藏指定类型的元素
-    delete entries['create.intermediate-event']; // 中间
-    delete entries['create.user-task']; // 用户任务
-    delete entries['create.user-sign-task']; // 会签任务
-    delete entries['create.subprocess-expanded'];// 子流程
-    delete entries['create.data-object'];// 数据对象
-    delete entries['create.data-store'];// 数据存储
-    delete entries['create.participant-expanded'];// 扩展流程
-    delete entries['create.group'];// 分组
+    // delete entries['create.intermediate-event']; // 中间
+    // delete entries['create.user-task']; // 用户任务
+    // delete entries['create.user-sign-task']; // 会签任务
+    // delete entries['create.subprocess-expanded'];// 子流程
+    // delete entries['create.data-object'];// 数据对象
+    // delete entries['create.data-store'];// 数据存储
+    // delete entries['create.participant-expanded'];// 扩展流程
+    // delete entries['create.group'];// 分组
     return entries;
   }
 }
